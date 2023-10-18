f596a50907eb2d45c8c24ba7e4e59f0d54ac0e42
MikeSyntax <autoglasgold@gmail.com>
2023-10-18 16:55:18 +0200

    Intro, Login und Info fertig

---
+++ app/src/main/java/com/example/modulabschlussandroid/secret/SecretData.kt
@@ -0,0 +1,7 @@
+package com.example.modulabschlussandroid.secret
+
+/* Dies ist der Konstruktor der Klasse SecretData und er erhält Parameter, welche beim Erstellen von SecretData-Objekten übergeben werden. */
+class SecretData(var password: String = "1234", var userName: String = "Mustermann", var email: String = "mustermann@web.de") {
+
+
+}
--- app/src/main/java/com/example/modulabschlussandroid/ui/ReadyLoginFragment.kt
+++ app/src/main/java/com/example/modulabschlussandroid/ui/ReadyLoginFragment.kt
@@ -5,24 +5,61 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.modulabschlussandroid.R
 import com.example.modulabschlussandroid.databinding.FragmentReadyLoginBinding
+import com.example.modulabschlussandroid.secret.SecretData
 
-class ReadyLoginFragment : Fragment() {
+
+//
+class ReadyLoginFragment() : Fragment() {
 
     private lateinit var binding: FragmentReadyLoginBinding
+
+    // Erstellen einer Instanz userProfile aus der Klasse SecretData
+    private val userProfile = SecretData()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-       binding = FragmentReadyLoginBinding.inflate(inflater,container,false)
+        binding = FragmentReadyLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.cvBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        binding.cvForward.setOnClickListener {
+            val user = binding.textInputUserAvatar.text.toString()
+            val authentification = binding.textInputUserPassword.text.toString()
+            val userEmail = binding.textInputUserEmail.text.toString()
+            if ((userProfile.userName == user
+
+
+
+                        && userProfile.password == authentification) || (userProfile.email == userEmail && userProfile.password == authentification)) {
+                findNavController().navigate(R.id.homeFragment)
+            } else {
+                when {
+                    userProfile.userName != user -> exceptionMessage("Leider falschen Benutzernamen eingegeben")
+                    userProfile.password != authentification -> exceptionMessage("Leider falsches Passwort eingegeben")
+                    userProfile.email != userEmail -> exceptionMessage("Leider falsche Email eingegeben")
+                    else -> exceptionMessage("Da ist etwas schiefgegangen")
+                }
+            }
+        }
+    }
+
+    private fun exceptionMessage(message: String) {
+        val duration = Toast.LENGTH_LONG
+        Toast.makeText(requireContext(), message, duration).show()
     }
 
 }
